7a8d63103f43c80797a5b76d2c1d2438d7ea68f4
ZanNen <github3746@ashleygc.co.uk>
2020-07-11 20:02:39 +0100

    Add skeleton div.py and corresponding test

---
+++ div.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+
+def div(a: int, b: int) -> Tuple[int, int]:
+    """
+    Calculate:
+    - a / b
+    - a % b
+    """
+
+    d = 0  # d: the result a/b
+    m = 0  # m: the result a%b
+
+    # TO BE IMPLEMENTED
+
+    return d, m
---
+++ test_div.py
@@ -0,0 +1,11 @@
+import div
+
+def test_div():
+    for a, b in [
+    	(0, 1234),
+        (1729, 10),
+        (5673892567438593, 5465),
+    ]:
+        d, m = div.div(a, b)
+        assert d == int(a / b)
+        assert m == a % b
