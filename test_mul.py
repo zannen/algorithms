0dd28976abe96c2469d997bf92409e77de0358a0
ZanNen <github3746@ashleygc.co.uk>
2020-07-11 20:01:09 +0100

    Ditch expected answers, trust "*" operator

--- test_mul.py
+++ test_mul.py
@@ -1,11 +1,11 @@
 import mul
 
 def test_mulPeasant():
-    for a, b, expected in [
-        (99, 0, 0),
-        (1, 1, 1),
-        (11, 3, 33),
-        (23958233, 5830, 139676498390),
-        (67548394032, 54325277, 3669585216693546864),
+    for a, b in [
+        (99, 0),
+        (1, 1),
+        (11, 3),
+        (23958233, 5830),
+        (67548394032, 54325277),
     ]:
-        assert mul.mulPeasant(a, b) == expected
+        assert mul.mulPeasant(a, b) == a * b
