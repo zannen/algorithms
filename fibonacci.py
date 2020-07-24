acee274c3d784d9d2046eea90e3e1b7aa304d0f8
ZanNen <github3746@ashleygc.co.uk>
2020-07-24 22:03:29 +0100

    Add fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+def fib1(n: int) -> int:
+    # TO BE IMPLEMENTED
+    return result
+
+
+def fib2(n: int) -> int:
+    # TO BE IMPLEMENTED
+    return result
---
+++ test_fibonacci.py
@@ -0,0 +1,27 @@
+import fibonacci as f
+
+answers = [
+    (0, 0),
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 3),
+    (5, 5),
+    (10, 55),
+    (20, 6765),
+    (30, 832040),
+]
+
+
+def test_fibonacci1():
+	# The slow function
+    for i, fib_i in answers:
+        assert f.fib1(i) == fib_i
+
+
+def test_fibonacci2():
+	# The quicker function
+    answers2 = answers
+    answers2.append((40, 102334155))
+    for i, fib_i in answers2:
+        assert f.fib2(i) == fib_i
