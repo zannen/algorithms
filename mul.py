0fbf7dab7188f52f9d2cae541b5aea6b1b58e726
ZanNen <github3746@ashleygc.co.uk>
2020-08-07 20:22:40 +0100

    Add mul

--- mul.py
+++ mul.py
@@ -2,7 +2,12 @@
 # https://en.wikipedia.org/wiki/Multiplication_algorithm#Binary_or_Peasant_multiplication
 def mulPeasant(a: int, b: int) -> int:
     result: int = 0
+    if a > b:
+        a, b = b, a
 
-    # TO BE IMPLEMENTED
-
+    while a >= 1:
+        if a % 2 == 1:
+            result += b
+        a >>= 1
+        b <<= 1
     return result
